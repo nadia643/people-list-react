2f4adc558ae5cb2e24aefffd22a992466c6af1f8
John Doe <nadia643@gmail.com>
2020-01-28 15:22:08 +0000

    first function written

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 function Greeting() {
-    return 
+    return (
+    <div> 
+        <h1>this is nadia and this is not my first react component, but it is my first rodeo </h1> 
+    </div>
+    );
+}
 
-};
+
+ReactDom.render(<Greeting />, document.getElementById('root'));
