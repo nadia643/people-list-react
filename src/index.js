ebba7f4b406b610238fbe0a1bf47ad650e0d7f7c
John Doe <nadia643@gmail.com>
2020-01-29 16:59:24 +0000

    made person list

--- src/index.js
+++ src/index.js
@@ -1,34 +1,30 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-// import './index.css';
+import './index.css';
 
-// function sayName(name) {
-//     console.log(name);
-// }
-// sayName("John");
-
-function People() {
+const Person = ({img, name, job, children}) => {
+    const url = `https://randomuser.me/api/portraits/thumb/men/75.jpg`
     return(
-        <section>
-            <Person />
-        </section>
-
-    )
-}
-
-
-
-const Person = (props) => {
-    console.log(props);
-
-    return(
-        <article>
-            <h1>John Doe</h1>
-            <p>Developer
-            </p>
-            <hr />
+        <article className="person">
+            <img src={url} alt="person" />
+            <h4>{name}</h4>
+            <h4>{job}</h4>
+            {children}
         </article>
     )
+
+}
+const PersonList = () => {
+    return(
+        <section className="person-list">
+            <Person img="34" name="Bob" job="Reggae King"/>
+            <Person img="22" name="Christina" job="Popstar"> 
+            <p>lorem20 paragraph</p>
+            </Person>
+            <Person img="56" name="Elizabeth" job="Queen"/>
+
+        </section>
+    )
 }
 
-ReactDom.render(<People />, document.getElementById('root'));
+ReactDom.render(<PersonList />, document.getElementById('root'));
