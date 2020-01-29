3707e9a62a5793d535a855a41fdb983ad4fb25ac
John Doe <nadia643@gmail.com>
2020-01-29 17:08:30 +0000

    bio done

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDom from 'react-dom';
 import './index.css';
 
 const Person = ({img, name, job, children}) => {
-    const url = `https://randomuser.me/api/portraits/thumb/men/75.jpg`
+    const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`
     return(
         <article className="person">
             <img src={url} alt="person" />
@@ -19,7 +19,11 @@ const PersonList = () => {
         <section className="person-list">
             <Person img="34" name="Bob" job="Reggae King"/>
             <Person img="22" name="Christina" job="Popstar"> 
-            <p>lorem20 paragraph</p>
+            <p>Singer-songwriter Christina Aguilera was born on December 18, 1980, 
+                in Staten Island, New York. Aguilera began her career as a cast member 
+                on The All New Mickey Mouse Club. She rapidly rose to stardom following 
+                the release of her hit single, "Genie in a Bottle," which earned her a 
+                Grammy Award for Best New Artist.</p>
             </Person>
             <Person img="56" name="Elizabeth" job="Queen"/>
 
